3d2edcf43dab0fb2ee63e1912008355c9395e687
Philip Cheek <maccsphilipcheek@gmail.com>
2017-02-26 16:53:40 -0800

    history brush/viewport

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var Game = function(canvasID){
 	this.camera = [0, 0];
 	this.background = new Background();
 	this.player = new Guy([0, 0]);
+	this.viewPort = new ViewPort(25);
 	this.scale;
 }
 
@@ -25,18 +26,21 @@ Game.prototype.start = function(){
 	var self = this;
 
 	this.player.listenForMovement();
-	this.background.buildMaze(90);
+	this.background.buildMaze(40);
 	window.requestAnimFrame(gameLoop);
+	this.scale = window.innerHeight/700;
 
 	function gameLoop(){
 		self.context.clearRect(0, 0, self.canvas.height, self.canvas.width);
 		self.canvas.height = window.innerHeight;
 		self.canvas.width = window.innerWidth;
 		self.scale = window.innerHeight/700;
+
 		self.context.save();
 		self.context.scale(self.scale, self.scale);
-		self.background.draw(self.context);
-		self.player.draw(self.context, self.scale);
+		var offset = self.viewPort.offset(self.player.coord, self.scale, self.context);
+		self.background.draw(self.context, offset);
+		self.player.draw(self.context, self.scale, offset);
 		self.context.restore();
 
 		window.requestAnimFrame(gameLoop)
@@ -58,17 +62,54 @@ var Guy = function(initCoord){
 		'width': 60,
 		'height': 60
 	}
+	this.history = [];
 }
 
-Guy.prototype.draw = function(ctx, scale){
+Guy.prototype.aCoord = function(){
+	this.dimen.width
+	this.dimen.height
+}
+
+Guy.prototype.draw = function(ctx, scale, offset){
+	var x = this.coord[0] - ((this.dimen.width)/2 * scale) - offset[0], 
+		y = this.coord[1] - ((this.dimen.height)/2 * scale) - offset[1]
+
+	this.handleHistory(ctx, offset);
+
 	ctx.save();
 	ctx.fillStyle = 'blue';
-	ctx.fillRect(this.coord[0], this.coord[1], this.dimen.width, this.dimen.height);
+	ctx.fillRect(x, y, this.dimen.width, this.dimen.height);
 	ctx.restore();
 
-	this.updateCoord(scale);
+	this.updateCoord(scale, offset);
+
 }
 
+Guy.prototype.handleHistory = function(ctx, offset){
+	var novel = true;
+   	
+   	ctx.save();
+   	ctx.fillStyle = 'pink';
+	for (var i = 0; i < this.history.length; i++){
+		var h = this.history[i];
+
+		ctx.beginPath();
+		ctx.arc(h[0] - offset[0], h[1] - offset[1], 15, 0, Math.PI * 2, true);
+		ctx.closePath();
+		ctx.fill();
+
+
+		if (Math.sqrt(Math.pow(this.coord[0] - h[0], 2) + Math.pow(this.coord[1] - h[1], 2)) < 3){
+			novel = false;
+		}
+	}
+
+	ctx.restore();
+
+	if (novel){ 
+		this.history.push([this.coord[0], this.coord[1]]); 
+	}
+}
 Guy.prototype.listenForMovement = function(){
 	var self = this;
 
@@ -77,12 +118,13 @@ Guy.prototype.listenForMovement = function(){
 	});
 };
 
-Guy.prototype.updateCoord = function(scale){
+Guy.prototype.updateCoord = function(scale, offset){
 	var maxDist = this.maxDistance,
-		coord = [
-			this.coord[0] + (this.dimen.width)/2 * scale,
-			this.coord[1] + (this.dimen.height)/2 * scale
-		], pointer = [this.pointer[0]/scale, this.pointer[1]/scale];
+		coord = this.coord,
+		pointer = [
+			this.pointer[0]/scale + offset[0],
+			this.pointer[1]/scale + offset[1]
+		], 
 
 		xDist = pointer[0] - coord[0],
 		yDist = pointer[1] - coord[1],
@@ -102,18 +144,19 @@ Guy.prototype.updateCoord = function(scale){
 
 var Background = function(){
 	this.tiles = [];
+	this.history = [];
 	this.start;
 	this.end;
 }
 
-Background.prototype.draw = function(ctx){
+Background.prototype.draw = function(ctx, offset){
 	ctx.save();
 	ctx.fillStyle = 'green';
 
 	for (var y = 0; y < this.tiles.length; y++){
 		for (var x = 0; x < this.tiles[y].length; x++){
 			if (this.tiles[y][x]){
-				ctx.fillRect(x * 5, y * 5, 5, 5);
+				ctx.fillRect((x * 110) - offset[0], (y * 110) - offset[1], 110, 110);
 			}
 		}
 	}
@@ -231,14 +274,12 @@ Maze.prototype.pickEnds = function(){
 		var sides = [[0, null], [this.size - 1, null], [null, 0], [null, this.size - 1]],
 			side = sides[Math.floor(Math.random() * sides.length)];
 
-		console.log("SIDE", side);
 		for (var i = 0; i < side.length; i++){
 			if (side[i] !== 0 && !side[i]){
 				side[i] = Math.floor(Math.random() * this.size);
 			}
 		}
 
-		console.log(side, this.size - 1);
 		if (this.map[side[1]][side[0]] && this.map[side[1]][side[0]].visited && !this.start){
 			this.start = side;
 		}else if (this.map[side[1]][side[0]] && this.map[side[1]][side[0]].visited){
@@ -250,6 +291,46 @@ Maze.prototype.pickEnds = function(){
 
 
 
+};
+
+var ViewPort = function(maxDist){
+	this.coord = [0, 0];
+	this.maxDist = maxDist;
+}
+
+ViewPort.prototype.offset = function(pCoord, scale, ctx){
+	var center = [
+		this.coord[0] + (window.innerWidth/2)/scale,
+		this.coord[1] + (window.innerHeight/2)/scale
+	];
+
+  ctx.save();
+  ctx.beginPath();
+  ctx.arc(center[0] - this.coord[0], center[1] - this.coord[1], this.maxDist * scale, 0, 2 * Math.PI);
+  ctx.arc(pCoord[0] - this.coord[0], pCoord[1] - this.coord[1], 5, 0, 2 * Math.PI)
+  ctx.stroke();
+  ctx.restore();
+
+	var maxDist = this.maxDist * scale;
+		xDist = pCoord[0] - center[0],
+		yDist = pCoord[1] - center[1],
+		distance = Math.sqrt(Math.pow(xDist,2) + Math.pow(yDist,2));
+
+		if (distance > maxDist){
+			var angle = Math.asin(yDist/distance);
+				xOffset = Math.cos(angle) * maxDist,
+				yOffset = Math.sin(angle) * maxDist;
+
+			if (pCoord[0] < center[0]){ xOffset *= -1; }
+
+			var mX = center[0] + xOffset;
+				mY = center[1] + yOffset;
+
+			this.coord[0] += pCoord[0] - mX
+			this.coord[1] += pCoord[1] - mY;
+		}
+
+	return this.coord;
 }
 
 function shuffle(a){
