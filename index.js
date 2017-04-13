82750a384ba44ed37e9bb16e82767b4164b8a401
Philip Cheek <maccsphilipcheek@gmail.com>
2017-04-12 20:24:52 -0700

    aesthetic

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const express = require('express'),
-	  path = require('path'),
-	  app = express(),
+      path = require('path'),
+      app = express(),
       helmet = require('helmet'),
-	  server = require('http').createServer(app),
+      server = require('http').createServer(app),
       bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -14,4 +14,4 @@ app.use(express.static(path.join(__dirname, '/client')));
 const instance = app.listen(process.env.port || 5000, function(){
   console.log('we\'re listening on port 5000');
 }),   
-	  io = require('./server/socketManager.js')(instance);
+      io = require('./server/socketManager.js')(instance);
